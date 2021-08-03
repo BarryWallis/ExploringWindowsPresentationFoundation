65f9560c49752c435bf32bbc31f1a3bb3febbf7d
Barry Wallis <barry.wallis@outlook.com>
2021-08-02 21:30:22 -0700

    Add project files.

---
+++ CalculateAreaWpf/MainWindow.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+
+namespace CalculateAreaWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+
+        private void TriangleAreaButton_Click(object sender, RoutedEventArgs e) => _ = MessageBox.Show("Triangle Area buttton clicked");
+
+        private void RectangleAreaButton_Click(object sender, RoutedEventArgs e) => _ = MessageBox.Show("Rectangle Area buttton clicked");
+
+        private void TrapezoidAreaButton_Click(object sender, RoutedEventArgs e) => _ = MessageBox.Show("Trapezoid Area buttton clicked");
+    }
+}
